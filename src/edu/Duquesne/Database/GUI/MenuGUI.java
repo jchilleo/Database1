7e7f4f34bd58cdb6ab97b6393ae2a280b46e883f
jchilleo <jchilleo2011@gmail.com>
2015-09-20 18:33:37 -0400

    Initial
    Pushing the first batch, and the start of the GUI

---
+++ src/edu/Duquesne/Database/GUI/MenuGUI.java
@@ -0,0 +1,51 @@
+package edu.Duquesne.Database.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+
+public class MenuGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuGUI frame = new MenuGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenuGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 609, 585);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(235, 232, 237));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBackground();
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+	}
+
+}
